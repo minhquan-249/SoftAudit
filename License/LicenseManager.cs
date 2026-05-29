0d67e10ffceda0d760b65fffc24ca42d816bd315
minhquan-249 <toikola2003@gmail.com>
2026-05-29 13:43:36 +0700

    Remove check license of Windows and AutoCAD

--- License/LicenseManager.cs
+++ License/LicenseManager.cs
@@ -1,7 +1,7 @@
 using SoftAudit.Core.Models;
-using SoftAudit.Config;
+// using SoftAudit.Config;
 using SoftAudit.License.Checkers;
-using System.Collections.Generic;
+// using System.Collections.Generic;
 
 namespace SoftAudit.License
 {
@@ -9,46 +9,47 @@ namespace SoftAudit.License
     {
         private readonly SoftwareRules _rules;
 
-        private readonly WindowsChecker _windows;
+        // private readonly WindowsChecker _windows;
         private readonly OfficeChecker _office;
-        private readonly AutoCADChecker _autocad;
+        // private readonly AutoCADChecker _autocad;
 
-        private string _windowsStatus = "Unknown";
+        // private string _windowsStatus = "Unknown";
         private string _officeStatus = "Unknown";
-        private string _autocadStatus = "Unknown";
+        // private string _autocadStatus = "Unknown";
 
         public LicenseManager(SoftwareRules rules)
         {
             _rules = rules;
 
-            _windows = new WindowsChecker();
+            // _windows = new WindowsChecker();
             _office = new OfficeChecker();
-            _autocad = new AutoCADChecker();
+            // _autocad = new AutoCADChecker();
         }
 
         public void Apply(List<Software> list)
         {
             // run once
-            _windowsStatus = _windows.CheckSystem();
+            // _windowsStatus = _windows.CheckSystem();
             _officeStatus = _office.CheckSystem();
-            _autocadStatus = _autocad.CheckSystem();
+            // _autocadStatus = _autocad.CheckSystem();
 
             foreach (var s in list)
             {
                 var type = DetectLicenseType(s);
 
-                if (type == "Windows")
-                {
-                    s.LicenseStatus = _windowsStatus;
-                }
-                else if (type == "Office")
+                // if (type == "Windows")
+                // {
+                //     s.LicenseStatus = _windowsStatus;
+                // }
+                // else
+                if (type == "Office")
                 {
                     s.LicenseStatus = _officeStatus;
                 }
-                else if (type == "AutoCAD")
-                {
-                    s.LicenseStatus = _autocadStatus;
-                }
+                // else if (type == "AutoCAD")
+                // {
+                //     s.LicenseStatus = _autocadStatus;
+                // }
             }
         }
 
