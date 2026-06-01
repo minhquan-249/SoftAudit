500f4b93ffa3a26957b90bbb0f5faf9d5ddad31f
minhquan-249 <toikola2003@gmail.com>
2026-06-01 09:20:01 +0700

    Update

--- Utils/NetworkHelper.cs
+++ Utils/NetworkHelper.cs
@@ -1,45 +1,56 @@
-using System.Net.NetworkInformation;
+using System;
+using System.Net;
 using System.Net.Sockets;
 
 namespace SoftAudit.Utils
 {
     public static class NetworkHelper
     {
-        public static string GetActiveIP()
+    public static string GetActiveIP()
+    {
+        try
         {
-            foreach (var ni in NetworkInterface.GetAllNetworkInterfaces())
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+
+            socket.Connect("8.8.8.8", 65530);
+
+            if (socket.LocalEndPoint is IPEndPoint endPoint)
             {
-                if (ni.OperationalStatus != OperationalStatus.Up)
-                    continue;
+                return endPoint.Address.ToString();
+            }
+        }
+        catch (SocketException)
+        {
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"[WARN] GetActiveIP failed: {ex.Message}");
+        }
 
-                var desc = ni.Description.ToLower();
+        return GetFallbackIP();
+    }
 
-                if (desc.Contains("virtual") ||
-                    desc.Contains("vpn") ||
-                    desc.Contains("vmware") ||
-                    desc.Contains("hyper-v") ||
-                    desc.Contains("fortinet") ||
-                    desc.Contains("bluetooth"))
-                    continue;
+        private static string GetFallbackIP()
+        {
+            try
+            {
+                var host = Dns.GetHostEntry(Dns.GetHostName());
 
-                var props = ni.GetIPProperties();
-
-                // must have default gateway (like your PS logic)
-                if (props.GatewayAddresses == null || props.GatewayAddresses.Count == 0)
-                    continue;
-
-                foreach (var addr in props.UnicastAddresses)
+                foreach (var addr in host.AddressList)
                 {
-                    if (addr.Address.AddressFamily != AddressFamily.InterNetwork)
+                    if (addr.AddressFamily != AddressFamily.InterNetwork)
                         continue;
 
-                    var ip = addr.Address.ToString();
+                    var ip = addr.ToString();
 
                     if (ip.StartsWith("127.") || ip.StartsWith("169.254"))
                         continue;
 
                     return ip;
                 }
+            }
+            catch
+            {
             }
 
             return "NoIP";
